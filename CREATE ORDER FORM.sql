91975e5cccd10d3e4e2d8f8275f9fc97087b3561
androidlutz <55858485+androidlutz@users.noreply.github.com>
2020-04-18 14:36:07 -0700

    header changes

---
+++ CREATE ORDER FORM.sql
@@ -0,0 +1,13 @@
+
+/*
+HEADER
+Program Name: Week Two Database
+Author: Andrew Lutz
+Class: CS162 Spring 2020
+Date: 04/17/2020
+Description: A database based on the orderform from Week One's exercise
+*/
+
+
+
+CREATE DATABASE OrderForm
--- ORDER FORM FORM Table.sql
+++ ORDER FORM FORM Table.sql
@@ -1,12 +1,3 @@
-/*
-HEADER
-Program Name: Week Two Database
-Author: Andrew Lutz
-Class: CS162 Spring 2020
-Date: 04/17/2020
-Description: A database based on the orderform from Week One's exercise
-*/
-
 
 USE ORDERFORM;
 
