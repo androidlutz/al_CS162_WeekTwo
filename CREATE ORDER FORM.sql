9f598af124f47f3a46a99d995ef50f286774db0a
androidlutz <55858485+androidlutz@users.noreply.github.com>
2020-04-20 15:17:18 -0700

    Update CREATE ORDER FORM.sql

--- CREATE ORDER FORM.sql
+++ CREATE ORDER FORM.sql
@@ -23,15 +23,14 @@ A123	Pencil	3
 B234	Eraser	1.5
 C345	Sharpener	8
 
-INSERT INTO form (CustomerNumber) SELECT CustomerNumber FROM customer;
 SELECT * FROM form;
 1234	0	1998-11-04
-INSERT INTO form (CustomerNumber) SELECT CustomerNumber FROM customer;
-	Note: When I queried the CustomerNumber from the Customer table to insert into the Form table, it would not insert.
-			the resulting value is 0 for customer number.  The syntax I used is above.  
        
 SELECT * FROM customer;
 1234	Billy	456 Hightower Street, Hong Kong, China
+
+SELECT * FROM orderline;
+1	1234	A123	100
 
 
 
