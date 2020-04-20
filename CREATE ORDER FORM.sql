53d580f14ba40be70014898ccec394a12aa20f9a
androidlutz <55858485+androidlutz@users.noreply.github.com>
2020-04-19 22:20:48 -0700

    update footer

--- CREATE ORDER FORM.sql
+++ CREATE ORDER FORM.sql
@@ -11,3 +11,28 @@ Description: A database based on the orderform from Week One's exercise
 
 
 CREATE DATABASE OrderForm
+
+
+
+
+/*
+Footer
+
+SELECT * FROM product;
+A123	Pencil	3
+B234	Eraser	1.5
+C345	Sharpener	8
+
+INSERT INTO form (CustomerNumber) SELECT CustomerNumber FROM customer;
+SELECT * FROM form;
+1234	0	1998-11-04
+INSERT INTO form (CustomerNumber) SELECT CustomerNumber FROM customer;
+	Note: When I queried the CustomerNumber from the Customer table to insert into the Form table, it would not insert.
+			the resulting value is 0 for customer number.  The syntax I used is above.  
+       
+SELECT * FROM customer;
+1234	Billy	456 Hightower Street, Hong Kong, China
+
+
+
+*/
